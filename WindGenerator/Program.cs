6ce083753fd0e0e27cefd987242a1cf3d352aea1
1594296529@qq.com <1594296529@qq.com>
2023-07-01 23:42:01 +0800

    wuhu test

--- WindGenerator/Program.cs
+++ WindGenerator/Program.cs
@@ -8,6 +8,7 @@ namespace WindGenerator
         [STAThread]
         static void Main()
         {
+            // wuhu~~~~~
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
